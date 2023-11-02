a0a88ff70022872ddb94329df7decdc1e8c8215e
11056027 <11056027@ntub.edu.tw>
2023-11-02 15:22:11 +0800

    123

---
+++ mysite/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+from mysite.models import Post
+from datetime import datetime
+
+# Create your views here.
+def homepage(request):
+    posts = Post.objects.all()
+    now = datetime.now()
+    return render(request,'index.html',locals())
+
+def showpost(request,slug):
+    post=Post.objects.get(slug=slug)
+    if post !=None:
+        return render(request,'post.html',locals())
+    else:
+        return redirect("/")
